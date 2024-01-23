b4ffb060346b62fd3e09a16a959821ee0f22de45
ElaineTavares <etsets.tavares@gmail.com>
2024-01-23 09:21:20 -0300

    nav bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const link1 = document.querySelector(".link1");
+const link2 = document.querySelector(".link2");
+const link3 = document.querySelector(".link3");
+const link4 = document.querySelector(".link4");
+
 const saudacao = 'document.querySelector("#saudacao");'
 console.log(saudacao);
 
@@ -12,6 +19,29 @@ const tecnologiaJs = document.querySelector('#tecnologias-js')
 
 const btnReact = document.querySelector('#react')
 const tecnologiaReact = document.querySelector('#tecnologias-react')
+
+
+hamburger.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+})
+
+link1.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+})
+
+link2.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+})
+
+link3.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+})
+
+link4.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+})
+
+
 
 
 
